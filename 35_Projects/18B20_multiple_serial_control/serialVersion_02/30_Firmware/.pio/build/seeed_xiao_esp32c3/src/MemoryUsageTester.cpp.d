fd5f4c8e1a537e42b534398f2cb83b7a27dfd33f
koreatogetherfactory <koreatogether@zoho.com>
2026-02-14 17:23:29 +0900

    change folder name

--- 35_Projects/18B20_multiple_serial_control/serialVersion_02/30_Firmware/.pio/build/seeed_xiao_esp32c3/src/MemoryUsageTester.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/src/MemoryUsageTester.cpp.o: \
- src/MemoryUsageTester.cpp src/MemoryUsageTester.h
--- 35_Projects/18B20_multiple_serial_control/serialVersion_02/30_Firmware/.pio/build/seeed_xiao_esp32c3/src/test_validation.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/src/test_validation.cpp.o: \
- src/test_validation.cpp
